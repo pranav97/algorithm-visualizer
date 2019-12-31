ae621e1cff7fb107f0efe1a72c66873a72c19496
Pranav Raghavan <gtalkpranav@gmail.com>
2019-12-30 17:54:22 -0800

    painting ok

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -27,15 +27,44 @@ class Grid extends React.Component {
         return rows;
     }
     onMouseDown = (rowCol) => {
-        console.log(rowCol)
         this.setState({mouseDownLocation : rowCol});
     }
 
+    paintGreenOrBlue = (start, end) => {
+        // find the lower of the two in rows, then find the higher of the two in rows and start iterating from lower to higher
+        console.log("mouse clicked down on", start);
+        console.log("mouse clicked down up", end);
+        var rows = this.state.boxRows
+
+        var i = start.row
+        
+        while(true) {
+            var j = start.col;
+            while(true) {
+                rows[i][j].backgroundColor = 'green';
+                this.setState({boxRows: rows})
+                if (j === end.col) 
+                    break
+                else if(j < end.col)
+                    j ++
+                else 
+                    j --;
+            }
+            if (i == end.row)
+                break
+            if (i < end.row)
+                i++;
+            else 
+                i--;
+        }
+        // console.log("state clicked down", this.state.mouseDownLocation);
+        // console.log("state clicked up", this.state.mouseUpLocation);
+    }
+
     onMouseUp = (rowCol) => {
-        console.log(rowCol)
         this.setState({mouseUpLocation:rowCol});
-        console.log("mouse clicked down on", this.state.mouseDownLocation);
-        console.log("mouse clicked down up", this.state.mouseUpLocation);
+        // set state is async so we are also passing the data coming into this function onto painGreenOrBlue
+        this.paintGreenOrBlue(this.state.mouseDownLocation, rowCol);
     }
     renderRows = () => {
         var rows = this.state.boxRows.map((row, rowInd) => {
