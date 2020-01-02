3e1920e5de56a2cd8ebb464c26689324651633e6
Pranav Raghavan <gtalkpranav@gmail.com>
2020-01-02 01:39:06 -0800

    sidebar perfecto

--- src/App.js
+++ src/App.js
@@ -2,41 +2,124 @@ import React from 'react';
 import Grid from './components/Grid';
 import SpeedSlider from './components/SpeedSlider';
 import './App.css';
-import Navbar from 'react-bootstrap/Navbar';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       boxRows: null,
-      speed: 3
-    }
+      speed: 3,
+      sidebarOpen: false,
+      height: window.innerHeight, 
+      width: window.innerWidth
+    };
+    this.updateDimensions = this.updateDimensions.bind(this);
   }
+
+  componentDidMount() {
+    console.log(this.state.height);
+    // Additionally I could have just used an arrow function for the binding `this` to the component...
+    window.addEventListener("resize", this.updateDimensions);
+  }
+
+  updateDimensions() {
+    this.setState({
+      height: window.innerHeight, 
+      width: window.innerWidth
+    });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions);
+  }
+
   updateSpeed = (val) => {
     this.setState({
       speed: val
     });
   }
 
+  getSidebarIconVisibility(){
+    var ret = "visible";
+    if (this.state.width > 700 ) {
+      ret = "hidden";
+    }
+    return ret;
+  }
 
+  // getCloseIconVisibility() {
+  //   var ret = "visible";
+  //   if (this.state.width > 700 ) {
+  //     ret = "hidden";
+  //   }
+  //   return ret;
+  // }
+
+  getSidebarClass() {
+    var ret = "ui right wide sidebar overlay";
+    if (this.state.width > 700 ) {
+      ret += " visible";
+    }
+    else {
+      if (this.state.sidebarOpen) {
+        ret += " visible";
+      }
+    }
+    return ret;
+  }
+  onClick = () => {
+    console.log("clicked");
+    if (this.state.sidebarOpen === true) {
+      this.setState({sidebarOpen: false});
+    }
+    else {
+      this.setState({sidebarOpen: true});
+    }
+  }
+
+  hideSidebar = () => {
+    this.setState({sidebarOpen: false});
+  }
 
   render() {
     return (
       <div className="App">
-        <Navbar bg="light" expand="lg">
-          <Navbar.Brand 
-            // href="#home"
-          >
-            Algorithm Visualizer
-          </Navbar.Brand>
-        </Navbar>
-        <div className="container">
-          <div className="viz">
-            <Grid maxRow="30" maxCol="20" speed={this.state.speed}/>
+        <div className="header">
+          <div 
+            key="sidebar"
+            className="ui top attached demo menu">
+            <h3 className="item"> Algorithm Visualizer</h3>
+            <div 
+              className="item" 
+              style={{visibility: this.getSidebarIconVisibility()}}
+              onClick={this.onClick}
+              >
+              <i 
+                className="sidebar icon" 
+                style={{visibility: this.getSidebarIconVisibility()}}
+              ></i>
+            </div>
           </div>
-          <nav className='controls'>
-            <SpeedSlider defaultValue={this.state.speed} updateSpeed={this.updateSpeed}/>
-          </nav>
+        </div>
+        <div className={this.getSidebarClass()}>
+          <div 
+            className="item"
+            onClick={this.hideSidebar}
+            style={{marginTop: '30px'}}
+          >
+            <i className="chevron right icon"></i>
+          </div>
+          <SpeedSlider
+            defaultValue={this.state.speed} 
+            updateSpeed={this.updateSpeed} />
+        </div>
+        <div className="dimmed pusher">
+          <div className="viz">
+          <Grid 
+              maxRow="30" 
+              maxCol="20" 
+              speed={this.state.speed}/>
+          </div>
         </div>
       </div>
     );
